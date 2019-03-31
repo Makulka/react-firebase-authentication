c5267f79a0dbbf0b9d17d061ab55302cf009b26e
Makulka <mmakulka@yahoo.com>
2019-03-31 15:52:59 +0100

    firebase set up

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navigation from './Navigation';
+import LandingPage from './Landing/LandingPage';
+import SignUpPage from './SignUp/SignUpPage';
+import SignInPage from './SignIn/SignInPage';
+import PasswordForgetPage from './PasswordForget/PasswordForgetPage';
+import HomePage from './Home/HomePage';
+import AccountPage from './Account/AccountPage';
+import AdminPage from './Admin/AdminPage';
+
+import * as ROUTES from '../constants/routes';
+
+const App = () => (
+  <Router>
+    <div>
+      <Navigation />
+
+      <hr />
+
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      {/* <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+      <Route path={ROUTES.HOME} component={HomePage} />
+      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+      <Route path={ROUTES.ADMIN} component={AdminPage} /> */}
+    </div>
+  </Router>
+);
+
+export default App;
---
+++ src/components/Landing/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return(
+        <div>
+            landing Page
+        </div>
+    );
+}
+
+export default LandingPage;
